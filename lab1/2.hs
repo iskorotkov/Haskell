d8fafe7e68cd224499f6c966a42f7a4bf71b65cf
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-01-24 21:31:15 +0500

    Reformat code with linter

--- lab1/2.hs
+++ lab1/2.hs
@@ -1,4 +1,4 @@
-main = print $ (_gcd 24 176)
+main = print (_gcd 24 176)
 
 _gcd a 0 = a
 _gcd m n = _gcd n (mod m n)
--- lab2/1.hs
+++ lab2/1.hs
@@ -1,16 +1,19 @@
-main = print $ (nearest [("a", "b", 5), ("a", "c", 4), ("b", "c", 3), ("x", "y", 3)])
+main =
+  print $ nearest [("a", "b", 5), ("a", "c", 4), ("b", "c", 3), ("x", "y", 3)]
 
 nearest [] = []
-nearest cities =
-    nearest' cities [first_cities_pair cities] (first_dist cities) where
-        first_cities_pair cities = get_cities (head cities) where
-            get_cities (a, b, c) = (a, b)
-        first_dist cities = dist (head cities) where
-            dist (a, b, c) = c
-        nearest' [] results dist = results
-        nearest' ((city1, city2, new_dist) : other_cities) results dist =
-            if new_dist < dist then
-                nearest' other_cities [(city1, city2)] new_dist
-            else if new_dist == dist then
-                nearest' other_cities ((city1, city2) : results) dist
-            else nearest' other_cities results dist
+nearest cities = nearest' cities [first_cities_pair cities] (first_dist cities)
+  where
+    first_cities_pair cities = get_cities (head cities)
+      where
+        get_cities (a, b, c) = (a, b)
+
+    first_dist cities = dist (head cities)
+      where
+        dist (a, b, c) = c
+
+    nearest' [] results dist = results
+    nearest' ((city1, city2, new_dist):other_cities) results dist
+      | new_dist < dist = nearest' other_cities [(city1, city2)] new_dist
+      | new_dist == dist = nearest' other_cities ((city1, city2):results) dist
+      | otherwise = nearest' other_cities results dist
