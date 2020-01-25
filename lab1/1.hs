447a62b3475261dede91c19675e65d5bb7b754ae
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-01-25 23:49:25 +0500

    Apply brittany formatting style

--- lab1/1.hs
+++ lab1/1.hs
@@ -1,8 +1,6 @@
 main = print $ conjuction 8 10
 
-conjuction n m = if m > n
-                 then 0
-                 else div (fact n) (fact m * fact (n - m))
-  where
-    fact 0 = 1
-    fact n = n * fact (n - 1)
+conjuction n m = if m > n then 0 else div (fact n) (fact m * fact (n - m))
+ where
+  fact 0 = 1
+  fact n = n * fact (n - 1)
--- lab2/1.hs
+++ lab2/1.hs
@@ -1,19 +1,16 @@
 main =
   print $ nearest [("a", "b", 5), ("a", "c", 4), ("b", "c", 3), ("x", "y", 3)]
 
-nearest [] = []
+nearest []     = []
 nearest cities = nearest' cities [first_cities_pair cities] (first_dist cities)
-  where
-    first_cities_pair cities = get_cities (head cities)
-      where
-        get_cities (a, b, c) = (a, b)
+ where
+  first_cities_pair cities = get_cities (head cities)
+    where get_cities (a, b, c) = (a, b)
 
-    first_dist cities = dist (head cities)
-      where
-        dist (a, b, c) = c
+  first_dist cities = dist (head cities) where dist (a, b, c) = c
 
-    nearest' [] results dist = results
-    nearest' ((city1, city2, new_dist):other_cities) results dist
-      | new_dist < dist = nearest' other_cities [(city1, city2)] new_dist
-      | new_dist == dist = nearest' other_cities ((city1, city2):results) dist
-      | otherwise = nearest' other_cities results dist
+  nearest' [] results dist = results
+  nearest' ((city1, city2, new_dist) : other_cities) results dist
+    | new_dist < dist  = nearest' other_cities [(city1, city2)] new_dist
+    | new_dist == dist = nearest' other_cities ((city1, city2) : results) dist
+    | otherwise        = nearest' other_cities results dist
