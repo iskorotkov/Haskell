efa3dba5a36f9473dc9d9648d49c4f084912800e
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-01-30 16:26:15 +0500

    Задача 3 в части 2

---
+++ lab2/3.hs
@@ -0,0 +1,9 @@
+main = print $ 1
+
+winner participants = winner' [] [1 .. participants] 1
+ where
+  winner' [x]  []       _   = x
+  winner' []   [x]      _   = x
+  winner' prev []       cnt = winner' [] prev cnt
+  winner' prev (x : xs) 5   = winner' prev xs 1
+  winner' prev (x : xs) cnt = winner' (prev ++ [x]) xs (cnt + 1)
