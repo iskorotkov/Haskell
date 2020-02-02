1cee0c7233fd1fa6768b80ef411d98842c138359
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-02 19:36:22 +0500

    Refactor lab2/2.hs

--- lab2/2.hs
+++ lab2/2.hs
@@ -1,9 +1,6 @@
 relatives []  person = []
 relatives all person = relatives' all [] [person]
  where
-  relatives'::[(String, String)] -> [String] -> [String] -> [String]
+  --relatives'::[(String, String)] -> [String] -> [String] -> [String]
   relatives' all rels [] = rels
-  relatives' all rels sons = relatives' all (rels++[s|(f,s)<-all, in_ f sons]) [s|(f,s)<-all, in_ f sons]
-    where
-      in_ :: String -> [String] -> Bool
-      in_ x lst = x `elem` lst
+  relatives' all rels sons = relatives' all (rels++[s|(f,s)<-all, f `elem` sons]) [s|(f,s)<-all, f `elem` sons]
