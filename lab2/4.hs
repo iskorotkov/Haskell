440fa07bb0f133ca8e3f2dc201b366787f0c4113
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-02 21:45:30 +0500

    Rename function in 2.4

--- lab2/4.hs
+++ lab2/4.hs
@@ -1,15 +1,16 @@
-maxHolidays holidays = foo [1 .. 12] 0 0
-  where
-    foo [] mv mi = mi
-    foo (n : ns) mv mi | count > mv = foo ns count n
-                       | otherwise  = foo ns mv mi
-        where count = length (filter (\(d, m) -> m == n) holidays)
+maxHolidays holidays = holidaysPerMonth [1 .. 12] 0 0
+ where
+  holidaysPerMonth [] mv mi = mi
+  holidaysPerMonth (n : ns) mv mi | count > mv = holidaysPerMonth ns count n
+                                  | otherwise  = holidaysPerMonth ns mv mi
+    where count = length (filter (\(d, m) -> m == n) holidays)
 
 
-maxHolidaysN holidays = reverse (foo [1 .. 12] 0 [])
-  where
-    foo [] mv mi = mi
-    foo (n : ns) mv mi | count > mv  = foo ns count [n]
-                       | count == mv = foo ns count (n : mi)
-                       | otherwise   = foo ns mv mi
-        where count = length (filter (\(d, m) -> m == n) holidays)
+maxHolidaysN holidays = reverse (holidaysPerMonth [1 .. 12] 0 [])
+ where
+  holidaysPerMonth [] mv mi = mi
+  holidaysPerMonth (n : ns) mv mi
+    | count > mv  = holidaysPerMonth ns count [n]
+    | count == mv = holidaysPerMonth ns count (n : mi)
+    | otherwise   = holidaysPerMonth ns mv mi
+    where count = length (filter (\(d, m) -> m == n) holidays)
