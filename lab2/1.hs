ba007137e2eb30068d9b1d869be6b8af96e8cc73
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-02 19:46:31 +0500

    Refactor lab2/1.hs

--- lab2/1.hs
+++ lab2/1.hs
@@ -1,14 +1,9 @@
 main =
   print $ nearest [("a", "b", 5), ("a", "c", 4), ("b", "c", 3), ("x", "y", 3)]
 
-nearest []     = []
-nearest cities = nearest' cities [first_cities_pair cities] (first_dist cities)
+nearest []                          = []
+nearest cities@((c1, c2, dist) : t) = nearest' cities [(c1, c2)] dist
  where
-  first_cities_pair cities = get_cities (head cities)
-    where get_cities (a, b, c) = (a, b)
-
-  first_dist cities = dist (head cities) where dist (a, b, c) = c
-
   nearest' [] results dist = results
   nearest' ((city1, city2, new_dist) : other_cities) results dist
     | new_dist < dist  = nearest' other_cities [(city1, city2)] new_dist
