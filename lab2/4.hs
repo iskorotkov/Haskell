a9dc91c6170930d749c4249ccc8d94070eef92bf
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-02 21:43:39 +0500

    Refactor 2.4 and add another version to support mutliple answers

--- lab2/4.hs
+++ lab2/4.hs
@@ -1,5 +1,15 @@
 maxHolidays holidays = foo [1 .. 12] 0 0
   where
-    foo []       mv mi = mi
-    foo (n : ns) mv mi = if count > mv then foo ns count n else foo ns mv mi
+    foo [] mv mi = mi
+    foo (n : ns) mv mi | count > mv = foo ns count n
+                       | otherwise  = foo ns mv mi
+        where count = length (filter (\(d, m) -> m == n) holidays)
+
+
+maxHolidaysN holidays = reverse (foo [1 .. 12] 0 [])
+  where
+    foo [] mv mi = mi
+    foo (n : ns) mv mi | count > mv  = foo ns count [n]
+                       | count == mv = foo ns count (n : mi)
+                       | otherwise   = foo ns mv mi
         where count = length (filter (\(d, m) -> m == n) holidays)
