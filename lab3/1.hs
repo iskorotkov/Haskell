3d9e334e4e9a9cac7c14f4e047f8709e6c62b253
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-03 14:01:16 +0500

    Refactor 3.1

--- lab3/1.hs
+++ lab3/1.hs
@@ -1,15 +1,12 @@
 data Tree =
-  Branch {value :: String, left :: Tree, right :: Tree}
-  | Leaf {value :: String}
+  Branch String Tree Tree
+  | Leaf String
 
-parseTerm :: [String] -> Tree
 parseTerm [x        ] = Leaf x
 parseTerm (l : x : r) = Branch x (parseTerm r) (Leaf l)
 
-parse :: String -> Tree
 parse s = parseExpr [] (reverse (words s))
  where
-  parseExpr :: [String] -> [String] -> Tree
   parseExpr l []      = parseTerm l
   parseExpr l (x : r) = if x == "+" || x == "-"
     then Branch x (parseExpr [] r) (parseTerm l)
@@ -22,18 +19,15 @@ instance Show Tree where
 postfix (Leaf x      ) = x
 postfix (Branch x l r) = postfix l ++ " " ++ postfix r ++ " " ++ x
 
-evalTree :: Tree -> Integer
 evalTree (Leaf x) = read x :: Integer
 evalTree (Branch x l r) | x == "+" = evalTree l + evalTree r
                         | x == "-" = evalTree l - evalTree r
                         | x == "*" = evalTree l * evalTree r
                         | x == "/" = evalTree l `div` evalTree r
 
-evalPostfix :: String -> Integer
 evalPostfix [] = 0
 evalPostfix s  = evalExpr [] (words s)
  where
-  evalExpr :: [Integer] -> [String] -> Integer
   evalExpr [x         ] []          = x
   evalExpr (y : x : xs) ("+" : ops) = evalExpr ((x + y) : xs) ops
   evalExpr (y : x : xs) ("-" : ops) = evalExpr ((x - y) : xs) ops
