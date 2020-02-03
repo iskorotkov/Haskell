4017f535a1728a4ca8928b3fb46e849ec2dd1e20
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-03 10:25:56 +0500

    Add postfix for 3.2

--- lab3/1.hs
+++ lab3/1.hs
@@ -2,22 +2,22 @@ data Tree a =
   Branch {value :: a, left :: (Tree a), right :: (Tree a)}
   | Leaf {value :: a}
 
-parseTerm :: [String] -> (Tree String)
+parseTerm :: [String] -> Tree String
 parseTerm [x        ] = Leaf x
 parseTerm (l : x : r) = Branch x (parseTerm r) (Leaf l)
 
-parse :: String -> (Tree String)
+parse :: String -> Tree String
 parse s = parseExpr [] (reverse (words s))
  where
-  parseExpr :: [String] -> [String] -> (Tree String)
+  parseExpr :: [String] -> [String] -> Tree String
   parseExpr l []      = parseTerm l
   parseExpr l (x : r) = if x == "+" || x == "-"
     then Branch x (parseExpr [] r) (parseTerm l)
     else parseExpr (l ++ [x]) r
 
---instance (Read a) => Read (Tree a) where
-    --read s = []
-
 instance (Show a) => Show (Tree a) where
   show (Leaf x      ) = show x
   show (Branch x l r) = "<" ++ show l ++ ">" ++ show x ++ "<" ++ show r ++ ">"
+
+postfix (Leaf x      ) = show x
+postfix (Branch x l r) = postfix l ++ " " ++ postfix r ++ " " ++ show x
